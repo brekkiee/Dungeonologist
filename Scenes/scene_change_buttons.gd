181f00f422da46accb1528e9043a9cb15e98db88
LuckyLoui-C <dan@dancampbell.com.au>
2024-07-04 14:02:55 +1000

    Placeholder_Scenes_Buttons
    1. Added place holder scene for Alchemy Lab & Monster Enclosure.
    2. Added a new UI scene for buttons, will be controlled with the Game Manager Singleton.
    3. Re-fix Book open close UI bug, need to keep mouse Filter to Ignore on non-clickable UI.

---
+++ Scenes/scene_change_buttons.gd
@@ -0,0 +1,13 @@
+extends Control
+
+### Using GameManager to change scenes
+### https://docs.godotengine.org/en/3.1/getting_started/step_by_step/singletons_autoload.html#custom-scene-switcher
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- Scripts/tweezers.gd
+++ Scripts/tweezers.gd
@@ -15,29 +15,5 @@ func _process(delta):
 	if Input.is_mouse_button_pressed(0):
 		picked_up = false
 
-#	var splinter = get_node(splinter_path)
-#	if splinter:
-#		var collision_shape = splinter.get_node("CollisionShape2D")
-#		if collision_shape:
-#			var shape = collision_shape.shape
-#			var splinter_rect = Rect2(collision_shape.global_position - shape.extents, shape.extents * 2)
-#			if splinter_rect.has_point(global_position):
-#				sprite.modulate = target_color  # Change color to green
-#			else:
-#				sprite.modulate = normal_color  # Change color back to normal
-
 func _on_mouse_exited():
 	picked_up = false
-
-#func _input(event):
-#	if event is InputEventMouseButton and event.pressed and picked_up:
-##		var splinter = get_node(splinter_path)
-#		if splinter and splinter.visible:
-#			var collision_shape = splinter.get_node("CollisionShape2D")
-#			if collision_shape:
-#				var shape = collision_shape.shape
-#				var splinter_rect = Rect2(collision_shape.global_position - shape.extents, shape.extents * 2)
-#				if splinter_rect.has_point(mouse_position):
-#					var monster = get_tree().get_root().get_node("Home/monster")
-#					if monster and monster.has_method("heal_monster"):
-#						monster.heal_monster()
