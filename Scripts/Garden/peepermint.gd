9e15f0630b745951f885699d34a8ba62ecdc78db
LuckyLoui-C <dan@dancampbell.com.au>
2024-08-15 17:10:12 +1000

    Updated new plants from base class

---
+++ Scripts/Garden/peepermint.gd
@@ -0,0 +1,6 @@
+extends PlantBase
+
+# Called when the node enters the scene tree for the first time.
+func _init():
+	growth_speed = 1
+	harvested_item_name = "peepermint_item"
---
+++ Scripts/Garden/sunflower.gd
@@ -0,0 +1,6 @@
+extends PlantBase
+
+# Called when the node enters the scene tree for the first time.
+func _init():
+	growth_speed = 3
+	harvested_item_name = "sunflower_item"
