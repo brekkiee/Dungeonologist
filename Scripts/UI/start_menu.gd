49c65e014aa43f7b5cad18eaeed2c25a6a207eea
brekkiee <aeraskimming@gmail.com>
2024-08-23 09:05:58 +1000

    Updated Menu Screen
    - Menu now works0
    - no longer overlays menu on gameplay

--- Scripts/UI/start_menu.gd
+++ Scripts/UI/start_menu.gd
@@ -2,16 +2,10 @@ extends Control
 
 @onready var main_window_scene = preload("res://Scenes/Windows/MainWindow.tscn")
 
-func _ready():
-	# Connect the "pressed" signal of the start button to a custom function
-	$StartMenuScreen/ButtonGridContainer/StartButton.connect("pressed", Callable(self, "_on_start_button_pressed"))
-	
-	# Connect a exit button
-	if $StartMenuScreen/ButtonGridContainer/ExitButton:
-		$StartMenuScreen/ButtonGridContainer/ExitButton.connect("pressed", Callable(self, "_on_exit_button_pressed"))
 	
 func _on_start_button_pressed():
 	GameManager.start_game()
+	hide()
 
 func _on_exit_button_pressed():
 	# Quit the game
