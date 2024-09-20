b97f9adb395bc62c33511de1eabccd16f8a14207
Joel-Bernoth <99691002+Joel-Bernoth@users.noreply.github.com>
2024-09-20 15:00:27 +1000

    Tooltips added and functioning

--- Scripts/Inventory/inventory_item.gd
+++ Scripts/Inventory/inventory_item.gd
@@ -3,7 +3,10 @@ extends Control
 # Item name assigned when instantiated/spawned
 var ItemName: String
 var ItemQuantity: int
+var ItemType: String
 
+@onready var item_tooltip_template = preload("res://Scenes/UI/InvItemTooltip.tscn")
+var tooltip = null
 # Follow mouse position
 func follow_mouse():
 	self.set_global_position(get_global_mouse_position())
@@ -12,11 +15,15 @@ func follow_mouse():
 func _on_button_pressed():
 	InventoryManager.icon_clicked(self, ItemName)
 
-
-
 func on_mouse_entered():
-	pass # Replace with function body.
-
+	tooltip = item_tooltip_template.instantiate()
+	tooltip.Config(self)
+	add_child(tooltip)
+	
 
 func on_mouse_exited():
-	pass # Replace with function body.
+	tooltip.queue_free()
+
+func hide_tooltip():
+	print("Hiding Tooltup")
+	tooltip.queue_free()
