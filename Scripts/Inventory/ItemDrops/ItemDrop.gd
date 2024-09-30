701f54365f05f4e1f96ef24693b7d14102aba6de
LuckyLoui-C <dan@dancampbell.com.au>
2024-09-30 12:36:16 +1000

    Updated New Sprites
    - Alchemy Lab
    - Added item icon sprites

--- Scripts/Inventory/ItemDrops/ItemDrop.gd
+++ Scripts/Inventory/ItemDrops/ItemDrop.gd
@@ -5,3 +5,8 @@ extends Resource
 @export_range(0.0,1.0) var drop_rate: float = 1.0
 @export var min_quantity: int = 1
 @export var max_quantity: int = 1
+@export var type: String
+@export var uses: Array[String] = []
+@export var drop_location: String
+# Unlock conditions: 0 = N/A, 1 = Floor 1, 2 = Floor 2, 3 = Floor 3
+@export var unlock_condition: int
